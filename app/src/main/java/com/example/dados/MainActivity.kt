085115da6d36894778cdeb22ed622b509891f1da
Antonio20055 <antonio.snono18@gmail.com>
2023-04-27 17:18:24 -0300

    atualizaçoes 27 de abril

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.dados
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dados.ui.theme.DadosTheme
 
@@ -22,22 +29,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    RolaDado()
                 }
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RolaDado(){
+    var resultado= 1
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+            ){
+            Image(
+                painter = painterResource(id = R.drawable.dice_1),
+                contentDescription = null
+
+            )
+           Button(
+               onClick = {
+                   resultado=(1..6).random()
+               }
+           ) {
+               Text(text = "Rolar")
+           }
+
+        }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DadosTheme {
-        Greeting("Android")
-    }
-}
